b01f6fcc4299ec8b0dfb402da886630c7e64b1d0
Matías Quezada <matias.traducciones@gmail.com>
2022-01-25 19:55:25 -0300

    modified code app-js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,11 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Any string");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".net", ".cl", ".io"];
+
